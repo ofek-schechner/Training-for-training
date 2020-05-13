ad2dd41682dc8bc45b96c2de43744558d9bba493
ofek-schechner <sofekd@gmail.com>
2020-05-13 17:42:19 +0200

    Feat(RabbitManager.cs)- Move relevant functions from rabbit class to manager. Feat(Rabbit.cs)- Create getters.

--- Watership/WatershipHill/RabbitManager.cs
+++ Watership/WatershipHill/RabbitManager.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace WatershipHill
 {
-    class Class1
+    public static class RabbitManager
     {
+        static private List<string> _maleNames = new List<string>() { "Bob", "Dan", "Greg" };
+        static private List<string> _femaleNames = new List<string>() { "Dana", "Anna", "Noa" };
+
+        // Generates a male rabbit's name
+        public static string generateMaleName()
+        {
+            Random random = new Random();
+
+            return RabbitManager._maleNames[random.Next(RabbitManager._maleNames.Count)];
+        }
+
+        // Generates a female rabbit's name
+        public static string generateFemaleName()
+        {
+            Random random = new Random();
+
+            return RabbitManager._femaleNames[random.Next(RabbitManager._femaleNames.Count)];
+        }
+
+        // Prints that a new rabbit was born
+        public static void declareBirth(Rabbit rabbit)
+        {
+            Console.WriteLine(rabbit.sex() + " " + rabbit.color() + " Bunny " + rabbit.name() + " Was Born!");
+        }
     }
 }

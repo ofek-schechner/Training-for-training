d8c78642e6691646ac0efdf1805c93c9403f1665
ofek-schechner <sofekd@gmail.com>
2020-05-20 15:00:23 +0200

    Feat(Enums.cs)- Add a file for enums.

--- Watership/WatershipHill/Enums.cs
+++ Watership/WatershipHill/Enums.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace WatershipHill
 {
+    // Names of possible colors
+    public enum Color
+    {
+        White,
+        Black,
+        Brown,
+        Spotted
+    }
+
     // Names of possible sexes
     public enum Sex
     {
